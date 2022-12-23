e02002848f2521d3e7e4f5f05ebde7c77b720865
falana sheriff <falanasheriffdeen@gmail.com>
2022-12-23 12:43:44 +0100

    work on homepage

--- frontend/cfc/src/components/Work.vue
+++ frontend/cfc/src/components/Work.vue
@@ -3,9 +3,28 @@
         <h1>How It Work</h1>
         <p>It's time to choose wisely your lerning platform. Bimble has professional <br /> mentors and instructors that always helps you to grow up your career.</p>
         <div class="main">
-            <div class="focus"></div>
-            <div class="focus"></div>
-            <div class="focus"></div>
+            <div class="focus">
+                <div class="purpose">
+                    <div class="purp"></div>
+                </div>
+                    <h3>Set Some Goals</h3>
+                    <p>We always stay organised for what we want to teach people. It is always good to be well organized.</p>
+            </div>
+            <div class="focus">
+                <div class="purpose">
+                    <div class="purp"></div>
+                </div>
+                    <h3>Join the Room</h3>
+                    <p>We always stay organised for what we want to teach people. It is always good to be well organized.</p>
+                </div>
+            <div class="focus">
+                
+                <div class="purpose">
+                    <div class="purp"></div>
+                </div>
+                    <h3>Keep Focus</h3>
+                    <p>We always stay organised for what we want to teach people. It is always good to be well organized.</p>
+            </div>
         </div>
     </section>
 </template>
@@ -47,5 +66,21 @@ section p {
     width: 350px;
     height: 500px;
     background: cornsilk;
+    padding: 30px;
+}
+.purpose {
+    width: 150px;
+    height: 150px;
+    background: plum;
+    border-radius: 20px;
+    margin: 20px auto 50px;
+    padding: 2px;
+}
+.purp {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    background: blue;
+    margin: 30px auto;
 }
 </style>
